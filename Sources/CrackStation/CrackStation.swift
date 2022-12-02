b712a9f3dce408b5c80863596b4a02ba61f636ca
croxe soon <croxe.soon@gmail.com>
2022-12-02 15:47:39 -0800

    fix bug of missing hash.plist

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -5,20 +5,21 @@ public class CrackStation: Decrypter {
     var passwords: [String : String] = [:]
 
     public required init() {
-        // The file format is apple's plist
-        let fileName = "hash.plist"
-
-        // URL will take the filename and add current directory
-        let URL = URL(fileURLWithPath: fileName)
-        
-        // Try to read the file from current directory
         do{
-            let data = try Data(contentsOf: URL)
-
-            // Check the nil type after read file from disk, and ensure the data is not corrupted
-            let tempPasswords = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as? [String:String]
-            if(tempPasswords == nil) {throw "Data corrupted"}
-            else {self.passwords = tempPasswords ?? [:]}
+            // The file format is apple's plist, URL will take the filename from resources bundle
+            let tempURL:URL? = Bundle.module.url(forResource: "hash", withExtension: "plist") 
+            if let hURL = tempURL {
+                print("hash.plist loaded")
+                // Load hashed password from hash.plist
+                let data = try Data(contentsOf: hURL )
+                // Check the nil type after read file from disk, and ensure the data is not corrupted
+                let tempPasswords = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as? [String:String]
+                if(tempPasswords == nil) {throw "Data corrupted"}
+                else {self.passwords = tempPasswords ?? [:]}
+            }    
+            else {
+                throw "URL not found, Library corrupted"
+            }
         }catch{
             // Print error if the file not founds
             print(error.localizedDescription)
